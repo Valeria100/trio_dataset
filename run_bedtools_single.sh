2f0ac0400de5962b2df77a6302fc496756a92a53
Valeria100 <valeria.bo01@gmail.com>
2015-12-14 14:09:03 +0000

    Update run_bedtools_single.sh
    single all

--- run_bedtools_single.sh
+++ run_bedtools_single.sh
@@ -1,101 +1,62 @@
-#---------------------------------- Bedtools - Installed only on the mac --------------------------------------------------#
+#!/bin/bash
+patients_index=(1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62);
 
-# run_bedtools.sh - in the input_bedtools_files folder - !!! Make sure you change the patients arrays in the sh file!!!
+	for i in `seq 0 ${#patients_index[@]}`;
+		do
+	 	printf "%s\n" "$i";
+		bedtools intersect -a ~/FinalCodeCNVs/input_bedtools_files/Benign/benign_pcnv_bed_patient_${patients_index[$i]}.bed -b ~/FinalCodeCNVs/input_bedtools_files/Benign/benign_dnacopy_bed_patient_${patients_index[$i]}.bed -wao > ~/FinalCodeCNVs/output_bedtools_files/Benign/benign_intersect_patient_wao_${patients_index[$i]}.txt;
+	done
 
-uni_pat_bg_tg <- uni_pat_bg[uni_pat_bg%in%uni_pat_tg]
-
-uni_pat_bg_tb <- uni_pat_bg[uni_pat_bg%in%uni_pat_tb]
-# If patient x is in bg but not in tb it will still return no intersection but if I select it before it will take less time.
-
-#--------------------------------------------------------------------------------------------------------------------------#
-
-
-#--------------------------------------------------------------------------------------------------------------------------#
-# Read the output of bedtools
-#--------------------------------------------------------------------------------------------------------------------------#
-
-source("functions_list.R")
-
-#BGpcnv vs BGdnacopy
-create_file_with_values(uni_pat_bg,
-                        "~/FinalCodeCNVs/output_bedtools_files/BG/bg_intersect_patient_wao_",
-                        "~/FinalCodeCNVs/input_bedtools_files/BG/bg_dnacopy_bed_values_patient_",
-                        "~/FinalCodeCNVs/input_bedtools_files/BG/bg_pcnv_bed_values_patient_",
-                        "~/FinalCodeCNVs/output_bedtools_files/BGvalues/bg_intersect_with_values_patient_")
-#TGpcnv vs TGdnacopy
-create_file_with_values(uni_pat_tg,
-                        "~/FinalCodeCNVs/output_bedtools_files/TG/tg_intersect_patient_wao_",
-                        "~/FinalCodeCNVs/input_bedtools_files/TG/tg_dnacopy_bed_values_patient_",
-                        "~/FinalCodeCNVs/input_bedtools_files/TG/tg_pcnv_bed_values_patient_",
-                        "~/FinalCodeCNVs/output_bedtools_files/TGvalues/tg_intersect_with_values_patient_")
-#TBpcnv vs TBdnacopy
-create_file_with_values(uni_pat_tb,
-                        "~/FinalCodeCNVs/output_bedtools_files/TB/tb_intersect_patient_wao_",
-                        "~/FinalCodeCNVs/input_bedtools_files/TB/tb_dnacopy_bed_values_patient_",
-                        "~/FinalCodeCNVs/input_bedtools_files/TB/tb_pcnv_bed_values_patient_",
-                        "~/FinalCodeCNVs/output_bedtools_files/TBvalues/tb_intersect_with_values_patient_")
-#BGpcnv vs TGpcnv
-create_file_with_values(uni_pat_bg_tg,
-                        "~/FinalCodeCNVs/output_bedtools_files/BG_TG/bg_tg_intersect_pcnv2_patient_wao_",
-                        "~/FinalCodeCNVs/input_bedtools_files/TG/tg_pcnv_bed_values_patient_",
-                        "~/FinalCodeCNVs/input_bedtools_files/BG/bg_pcnv_bed_values_patient_",
-                        "~/FinalCodeCNVs/output_bedtools_files/BG_TGvalues/output_bg_tg_intersect_pcnv2_with_values_patient_")
-#BGpcnv vs TBpcnv
-create_file_with_values(uni_pat_bg_tb,
-                        "~/FinalCodeCNVs/output_bedtools_files/BG_TB/bg_tb_intersect_pcnv2_patient_wao_",
-                        "~/FinalCodeCNVs/input_bedtools_files/TB/tb_pcnv_bed_values_patient_",
-                        "~/FinalCodeCNVs/input_bedtools_files/BG/bg_pcnv_bed_values_patient_",
-                        "~/FinalCodeCNVs/output_bedtools_files/BG_TBvalues/output_bg_tb_intersect_pcnv2_with_values_patient_")
-
-#BGpcnv vs TGdnacopy
-create_file_with_values(uni_pat_bg_tg,
-                        "~/FinalCodeCNVs/output_bedtools_files/BG_TG/bg_tg_intersect_pcnvDNAc_patient_wao_",
-                        "~/FinalCodeCNVs/input_bedtools_files/TG/tg_dnacopy_bed_values_patient_",
-                        "~/FinalCodeCNVs/input_bedtools_files/BG/bg_pcnv_bed_values_patient_",
-                        "~/FinalCodeCNVs/output_bedtools_files/BG_TGvalues/output_bg_tg_intersect_pcnvDNAc_with_values_patient_")
-#BGpcnv vs TBdnacopy
-create_file_with_values(uni_pat_bg_tb,
-                        "~/FinalCodeCNVs/output_bedtools_files/BG_TB/bg_tb_intersect_pcnvDNAc_patient_wao_",
-                        "~/FinalCodeCNVs/input_bedtools_files/TB/tb_dnacopy_bed_values_patient_",
-                        "~/FinalCodeCNVs/input_bedtools_files/BG/bg_pcnv_bed_values_patient_",
-                        "~/FinalCodeCNVs/output_bedtools_files/BG_TBvalues/output_bg_tb_intersect_pcnvDNAc_with_values_patient_")
+	for i in `seq 0 ${#patients_index[@]}`;
+		do
+	 	printf "%s\n" "$i";
+		bedtools intersect -a ~/FinalCodeCNVs/input_bedtools_files/Germline/germline_pcnv_bed_patient_${patients_index[$i]}.bed -b ~/FinalCodeCNVs/input_bedtools_files/Germline/germline_dnacopy_bed_patient_${patients_index[$i]}.bed -wao > ~/FinalCodeCNVs/output_bedtools_files/Germline/germline_intersect_patient_wao_${patients_index[$i]}.txt;
+	done
 
 
-#---------------------------------------------------------------------------------------------------------------------------#
-#  Analyse the txt file and build the contingency table
-#---------------------------------------------------------------------------------------------------------------------------#
-
-which_patients_pcnv(output_penncnv_bg,uni_pat_bg,"~/FinalCodeCNVs/output_bedtools_files/BG_pcnv_patients_chrs.csv")
-which_patients_pcnv(output_penncnv_tg,uni_pat_tg,"~/FinalCodeCNVs/output_bedtools_files/TG_pcnv_patients_chrs.csv")
-which_patients_pcnv(output_penncnv_tb,uni_pat_tb,"~/FinalCodeCNVs/output_bedtools_files/TB_pcnv_patients_chrs.csv")
+	for i in `seq 0 ${#patients_index[@]}`;
+		do
+	 	printf "%s\n" "$i";
+		bedtools intersect -a ~/FinalCodeCNVs/input_bedtools_files/Tumour/tumour_pcnv_bed_patient_${patients_index[$i]}.bed -b ~/FinalCodeCNVs/input_bedtools_files/Tumour/tumour_dnacopy_bed_patient_${patients_index[$i]}.bed -wao > ~/FinalCodeCNVs/output_bedtools_files/Tumour/tumour_intersect_patient_wao_${patients_index[$i]}.txt;
+	done
 
 
-setwd("~/FinalCodeCNVs")
 
-source("functions_list.R")
 
-create_contingency(uni_pat_bg,
-                   "~/FinalCodeCNVs/output_bedtools_files/BGvalues/bg_intersect_with_values_patient_",
-                   "~/FinalCodeCNVs/output_bedtools_files/BG_contingency.csv")
-create_contingency(uni_pat_tg,
-                   "~/FinalCodeCNVs/output_bedtools_files/TGvalues/tg_intersect_with_values_patient_",
-                   "~/FinalCodeCNVs/output_bedtools_files/TG_contingency.csv")
-create_contingency(uni_pat_tb,
-                   "~/FinalCodeCNVs/output_bedtools_files/TBvalues/tb_intersect_with_values_patient_",
-                   "~/FinalCodeCNVs/output_bedtools_files/TB_contingency.csv")
 
-#----------------------------------------------------------------------------------------------
 
-# Read the compare files and add the corresponding values
 
-create_contingency_compare(uni_pat_bg_tg, 
-                           "~/FinalCodeCNVs/output_bedtools_files/BG_TGvalues/output_bg_tg_intersect_pcnv2_with_values_patient_",
-                           "~/FinalCodeCNVs/output_bedtools_files/BG_TGvalues/output_bg_tg_intersect_pcnvDNAc_with_values_patient_",
-                           "~/FinalCodeCNVs/output_bedtools_files/contingency_bg_tg.csv", 
-                           c("TG_CN=1", "TG_CN=3", "TG_CN=4","TG-Gain=1", "TG-Loss=-1", "TG-Gain=0 , TG-Loss=0"))
+	for i in `seq 0 ${#patients_index[@]}`;
+		do
+	 	printf "%s\n" "$i";
+		bedtools intersect -a ~/FinalCodeCNVs/input_bedtools_files/Benign/benign_pcnv_bed_patient_${patients_index[$i]}.bed -b ~/FinalCodeCNVs/input_bedtools_files/Germline/germline_pcnv_bed_patient_${patients_index[$i]}.bed -wao > ~/FinalCodeCNVs/output_bedtools_files/Benign_Germline/ben_germ_intersect_pcnv2_patient_wao_${patients_index[$i]}.txt;
+		bedtools intersect -a ~/FinalCodeCNVs/input_bedtools_files/Benign/benign_pcnv_bed_patient_${patients_index[$i]}.bed -b ~/FinalCodeCNVs/input_bedtools_files/Germline/germline_dnacopy_bed_patient_${patients_index[$i]}.bed -wao > ~/FinalCodeCNVs/output_bedtools_files/Benign_Germline/ben_germ_intersect_pcnvDNAc_patient_wao_${patients_index[$i]}.txt;
+	done
 
-create_contingency_compare(uni_pat_bg_tb, 
-                           "~/FinalCodeCNVs/output_bedtools_files/BG_TBvalues/output_bg_tb_intersect_pcnv2_with_values_patient_",
-                           "~/FinalCodeCNVs/output_bedtools_files/BG_TBvalues/output_bg_tb_intersect_pcnvDNAc_with_values_patient_",
-                           "~/FinalCodeCNVs/output_bedtools_files/contingency_bg_tb.csv", 
-                           c("TB_CN=1", "TB_CN=3", "TB_CN=4","TB-Gain=1", "TB-Loss=-1", "TB-Gain=0 , TB-Loss=0"))
+
+
+
+	for i in `seq 0 ${#patients_index[@]}`;
+		do
+	 	printf "%s\n" "$i";
+		bedtools intersect -a ~/FinalCodeCNVs/input_bedtools_files/Benign/benign_pcnv_bed_patient_${patients_index[$i]}.bed -b ~/FinalCodeCNVs/input_bedtools_files/Tumour/tumour_pcnv_bed_patient_${patients_index[$i]}.bed -wao > ~/FinalCodeCNVs/output_bedtools_files/Benign_Tumour/ben_tum_intersect_pcnv2_patient_wao_${patients_index[$i]}.txt;
+		bedtools intersect -a ~/FinalCodeCNVs/input_bedtools_files/Benign/benign_pcnv_bed_patient_${patients_index[$i]}.bed -b ~/FinalCodeCNVs/input_bedtools_files/Tumour/tumour_dnacopy_bed_patient_${patients_index[$i]}.bed -wao > ~/FinalCodeCNVs/output_bedtools_files/Benign_Tumour/ben_tum_intersect_pcnvDNAc_patient_wao_${patients_index[$i]}.txt;
+	done
+
+
+
+
+
+	for i in `seq 0 ${#patients_index[@]}`;
+		do
+	 	printf "%s\n" "$i";
+		bedtools intersect -a ~/FinalCodeCNVs/input_bedtools_files/Tumour/tumour_pcnv_bed_patient_${patients_index[$i]}.bed -b ~/FinalCodeCNVs/input_bedtools_files/Germline/germline_pcnv_bed_patient_${patients_index[$i]}.bed -wao > ~/FinalCodeCNVs/output_bedtools_files/Tumour_Germline/tum_germ_intersect_pcnv2_patient_wao_${patients_index[$i]}.txt;
+		bedtools intersect -a ~/FinalCodeCNVs/input_bedtools_files/Tumour/tumour_pcnv_bed_patient_${patients_index[$i]}.bed -b ~/FinalCodeCNVs/input_bedtools_files/Germline/germline_dnacopy_bed_patient_${patients_index[$i]}.bed -wao > ~/FinalCodeCNVs/output_bedtools_files/Tumour_Germline/tum_germ_intersect_pcnvDNAc_patient_wao_${patients_index[$i]}.txt;
+	done
+
+
+
+
+
+
+
